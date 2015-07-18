8a0c9250a27ea1b3ebb55be8c012e42ef4debba5
b3rnix <bpanarello@gmail.com>
2015-07-18 20:51:48 -0300

    Lexer rules

---
+++ python/lexer_rules.py
@@ -0,0 +1,54 @@
+tokens = [
+   'NUM',
+   'CONST',
+   'EQUALS',
+   'CNAME',
+   'SEMICOLON',
+   'VOZBEGIN',
+   'LEFTPAR',
+   'RIGHTPAR',
+   'LEFTCURL',
+   'RIGHTCURL',
+   'COMPASBEGIN',
+   'COMPASHEADERBEGIN',
+   'LOOPBEGIN',
+   'SLASH',
+   'NOTEBEGIN',
+   'SHAPE',
+   'COMMA',
+   'NEWLINE'
+]
+
+def t_NUM(token):
+	r"[1-9][0-9]*"
+	token.value = int(token.value)
+	return token
+
+
+#Chequear regexs con: https://regex101.com/#python
+# Tener en cuenta poner las expresiones que matchean strings mas largos primero cuando hay ambiguedad.
+t_CONST = r"(const)",
+t_EQUALS = r"\=",
+t_SEMICOLON = r"\;",
+t_VOZBEGIN = r"(voz)",
+t_LEFTPAR = r"\(",
+t_RIGHTPAR =r"\)",
+t_LEFTCURL =r"\{",
+t_RIGHTCURL =r"\}",
+t_COMPASHEADERBEGIN =r"(#compas)",
+t_COMPASBEGIN =r"(compas)",
+t_LOOPBEGIN =r"(repetir)",
+t_SLASH =r"/",
+t_NOTEBEGIN =r"(nota)",
+t_SHAPE = r"(blanca|negra|redonda|semicorchea|corchea|semifusa|fusa)",
+t_NOTENAME=r"(do|re|mi|fa|sol|la|si)?(\+|\-)",
+t_COMMA =r"\,",
+t_CNAME =r"([a-z]|[A-Z])+"
+
+def t_error(token):
+    message = "Token desconocido:"
+    message += "\ntype:" + token.type
+    message += "\nvalue:" + str(token.value)
+    message += "\nline:" + str(token.lineno)
+    message += "\nposition:" + str(token.lexpos)
+    raise Exception(message)
