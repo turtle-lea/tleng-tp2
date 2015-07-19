4d9129dfb1d793936268733698911431c212ff01
b3rnix <bpanarello@gmail.com>
2015-07-18 21:43:04 -0300

    Correccion regex num

--- python/lexer_rules.py
+++ python/lexer_rules.py
@@ -1,41 +1,56 @@
-H-> {TEMPO}{COMPASHEADER}{CONST}{VOICELIST}
-TEMPO-> {#tempo}{space}{shape}{space}{num}{newline}
+tokens = [
+   'NUM',
+   'CONST',
+   'EQUALS',
+   'CNAME',
+   'SEMICOLON',
+   'VOZBEGIN',
+   'LEFTPAR',
+   'RIGHTPAR',
+   'LEFTCURL',
+   'RIGHTCURL',
+   'COMPASBEGIN',
+   'COMPASHEADERBEGIN',
+   'LOOPBEGIN',
+   'SLASH',
+   'NOTEBEGIN',
+   'SHAPE',
+   'COMMA',
+   'NEWLINE'
+]
 
-COMPASHEADER-> {compasheaderbegin}{space}{num}{slash}{num}{newline}
-
-CONST-> {const}{space}{cname}{equals}{cname}{semicolon}{newline}CONST
-CONST-> {const}{space}{cname}{equals}{num}{semicolon}{newline}CONST
-CONST-> {const}{space}{cname}{equals}{cname}{semicolon}{newline}
-CONST-> {const}{space}{cname}{equals}{num}{semicolon}{newline}
 
 
-VOICELIST->{VOICE}{VOICELIST}
-VOICELIST->{VOICE}
 
-VOICE->{vozbegin}{leftpar}{num}{rightpar}{leftcurl}{VOICECONTENT}{rightcurl}
-VOICE->{vozbegin}{leftpar}{const}{rightpar}{leftcurl}{VOICECONTENT}{rightcurl}
+#Chequear regexs con: https://regex101.com/#python
+# Tener en cuenta poner las expresiones que matchean strings mas largos primero cuando hay ambiguedad.
+t_CONST = r"(const)",
+t_EQUALS = r"\=",
+t_SEMICOLON = r"\;",
+t_VOZBEGIN = r"(voz)",
+t_LEFTPAR = r"\(",
+t_RIGHTPAR =r"\)",
+t_LEFTCURL =r"\{",
+t_RIGHTCURL =r"\}",
+t_COMPASHEADERBEGIN =r"(#compas)",
+t_COMPASBEGIN =r"(compas)",
+t_LOOPBEGIN =r"(repetir)",
+t_SLASH =r"/",
+t_NOTEBEGIN =r"(nota)",
+t_SHAPE = r"(blanca|negra|redonda|semicorchea|corchea|semifusa|fusa)",
+t_NOTENAME=r"(do|re|mi|fa|sol|la|si)?(\+|\-)",
+t_COMMA =r"\,",
+t_CNAME =r"([a-z]|[A-Z])+([0-9][a-z][A-Z])*"
 
-VOICECONTENT -> {COMPAS}
-VOICECONTENT -> {COMPASLOOP}
-VOICECONTENT -> {COMPAS}{VOICECONTENT}
-VOICECONTENT -> {COMPASLOOP}{VOICECONTENT}
+def t_NUM(token):
+	r"[1-9][0-9]*"
+	token.value = int(token.value)
+	return token
 
-COMPAS->{compasbegin}{leftcurl}{NOTELIST}{rightcurl}
-
-COMPASLOOP->{loopbegin}{leftpar}{cname}{rightpar}{leftcurl}{COMPASLIST}{rightcurl}
-COMPASLOOP->{loopbegin}{leftpar}{num}{rightpar}{leftcurl}{COMPASLIST}{rightcurl}
-
-COMPASLIST->{COMPAS}
-COMPASLIST->{COMPAS}{COMPASLIST}
-
-NOTELIST->{NOTE}
-NOTELIST->{NOTE}{NOTELIST}
-
-NOTE->{notebegin}{leftpar}{notename}{comma}{num}{comma}{shape}{rightpar}{semicolon}
-NOTE->{notebegin}{leftpar}{notename}{comma}{const}{comma}{shape}{rightpar}{semicolon}
-
-COMMENT -> {commenttoken}{anything}{newline}
-COMMENT -> {commenttoken}{anything}{eof}
-
-// Donde se pueden usar las constantes?
-// Los nombres de las constantes pueden empezar CON numeros?
+def t_error(token):
+    message = "Token desconocido:"
+    message += "\ntype:" + token.type
+    message += "\nvalue:" + str(token.value)
+    message += "\nline:" + str(token.lineno)
+    message += "\nposition:" + str(token.lexpos)
+    raise Exception(message)
