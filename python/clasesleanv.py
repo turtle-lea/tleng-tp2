33538dffcc0e1badb83ba64f579ef10a9b75a03e
Turtle lea <leandro.matayoshi@gmail.com>
2015-07-23 03:18:35 -0300

    Puntillo calculado utilizando 3.0/2.0

--- python/clasesleanv.py
+++ python/clasesleanv.py
@@ -105,7 +105,7 @@ class Note(Expression):
 		self._duration = duration_from_shape(shape)
 
 		if puntillo:
-			self._duration = self._duration*(3/2)
+			self._duration = self._duration*(3.0/2.0)
 
 
 	def getOctave(self):
